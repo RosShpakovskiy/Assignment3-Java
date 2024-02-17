0f7b9ad0915e4ade5cd96bd701e6900c5cefc0d5
RosShpakovskiy <153474253+RosShpakovskiy@users.noreply.github.com>
2024-02-17 20:39:29 +0600

    book table

---
+++ book.sql
@@ -0,0 +1,20 @@
+SELECT * FROM book
+
+INSERT INTO book (name, genre, price)
+VALUES 
+('Jane Eyre', 'Romance', 2700),
+('Wuthering Heights', 'Romance', 1600),
+('Pride & Prejudice', 'Romance', 1500),
+('It Ends With Us', 'Romance', 7500),
+('The Notebook', 'Romance', 3000),
+('The Night Circus', 'Fantasy', 5800),
+('Ozma of Oz', 'Fantasy', 4000),
+('Myst: The Book of Atrus', 'Fantasy', 16000),
+('A Hero Born', 'Fantasy', 7200),
+('Orbit The Fifth Season', 'Fantasy', 6300),
+('Carmilla', 'Horror', 5800),
+('Collected Ghost Stories', 'Horror', 6800),
+('I Am Legend', 'Horror', 3700),
+('Psycho', 'Horror', 6700),
+('Mongrels', 'Horror', 5200);
+
